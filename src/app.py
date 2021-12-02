4acadb44e0fc112aa68f79753686f293b9100961
mclaralvs <mclaralvs@gmail.com>
2021-12-02 10:44:00 -0300

    Alterações para uso de templates e ajuste no css.

--- src/app.py
+++ src/app.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template
+from jinja2 import Template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
- return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/sobre-mim')
 def about():
- return render_template('about.html')
+    return render_template('about.html')
 
 @app.route('/academico')
 def academic():
- return render_template('academic.html')
+    return render_template('academic.html')
 
 @app.route('/contatos')
 def contact():
- return render_template('contact.html')
+    return render_template('contact.html')
+
+@app.route('/teste')
+def teste():
+    return render_template('teste.html')
+  
 
 if __name__ == '__main__':
  app.run()
